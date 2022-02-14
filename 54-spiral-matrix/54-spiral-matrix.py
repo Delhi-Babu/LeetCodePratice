b72607920549228f5e76e126c9029fb5bf6dd33a
DONofDESTROY <47606472+DONofDESTROY@users.noreply.github.com>
2022-02-14 16:21:31 +0530

    Time: 38 ms (56.60%), Space: 13.9 MB (95.86%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def helper(self,matrix,i,j,res):
+        if i < 0 or i >= len(matrix) or j < 0 or j>=len(matrix[0]) or matrix[i][j] == '#':  return
+
+        res.append(matrix[i][j])
+        matrix[i][j] = '#'
+
+
+        if j>=i or i == j+1:
+            self.helper(matrix,i,j+1,res)
+        self.helper(matrix,i+1,j,res)
+        self.helper(matrix,i,j-1,res)
+        self.helper(matrix,i-1,j,res)
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = [] 
+        self.helper(matrix,0,0,res)
+        return res
+            
+        
